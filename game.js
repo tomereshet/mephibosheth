8aad423e8e53ceaac132b664ce8daf1e4d03809f
Dr. Tomer Eshet <dr@dr-Inspiron-N5010.(none)>
2014-03-24 19:08:49 +0200

    bugfix, design

--- game.js
+++ game.js
@@ -4,6 +4,9 @@ var INITIAL_VALUES = [2,3,5,7];
 var div;
 var board;
 var visited = [];
+var longPress = undefined;
+var longPressTimeout;
+var inAction = false;
 function Square() {}
 function Board(height,width) {
     this.height = height;
@@ -48,16 +51,64 @@ function createSquareElement(i,j,k) {
     if (k == undefined) {
 	k = 0;
     }
-    var square = document.createElement('a');
-    square.setAttribute('style','position:absolute; top:'+30*(i-k)+'px; left:'+30*j+'px; width: 30px; text-align: center;');
+    var square = document.createElement('div');
+    square.setAttribute('style','position:absolute; top:'+30*(i-k)+'px; left:'+30*j+'px; width: 28px; height: 28px; color: white; text-align: center; line-height: 30px; vertical-align: middle; background-color: rgb('+(board.grid[i][j]*102)%255+",128,"+(board.grid[i][j]*193)%255+");");
     square.setAttribute('id','row'+i+'col'+j);
-    square.setAttribute('onclick','clickSquare('+i+','+j+')');
+    square.setAttribute('onmouseup','clickSquare('+i+','+j+')');
+    square.setAttribute('onmousedown','mousedown('+i+','+j+')');
+    square.setAttribute('onmouseout','mouseout()');
+    var span = document.createElement('span');
+    span.setAttribute('class','text');
     var text = document.createTextNode(board.grid[i][j]);
-    square.appendChild(text);
+    span.appendChild(text);
+    square.appendChild(span);
     div.append(square);
     
 }
+function mouseout() {
+    if (longPressTimeout != undefined){
+	clearTimeout(longPressTimeout);
+    }
+}
+function mousedown(i,j) {
+    if (longPressTimeout == undefined) {
+	longPressTimeout = setTimeout(function(){squareAction(i,j); longPressTimeout = undefined;},1500);
+    }
+}
+function squareActionDone() {
+    if (longPress != undefined) {
+	$('#mainCircle').animate({
+		width: '1px',
+		    height: '1px',
+		    left: '15px',
+		    top: '15px'}, 200, function(){$('#mainCircle').remove()});
+	$('#row'+longPress[0]+'col'+longPress[1]+'>.text').animate({
+		fontSize: '100%'}, 200);
+	longPress = undefined;
+    }
+}
+function squareAction(i,j) {
+    if (longPress != undefined) {
+	return;
+    }
+    if (longPressTimeout != undefined){
+	clearTimeout(longPressTimeout);
+    }
+    longPress = [i,j];
+    $('#row'+i+'col'+j).append('<div class="circle" id="mainCircle" style="width: 1px; height: 1px; position: absolute; left: 15px; top: 15px; background-color: rgb(255,200,0);">');
+    $('#mainCircle').animate({
+	    width: '40px',
+		height: '40px',
+		left: '-6px',
+		top: '-6px'}, 200);
+    $('#row'+i+'col'+j+'>.text').animate({
+	    fontSize: '150%'}, 200);
+}
 function clickSquare(x,y) {
+    if (longPress != undefined || inAction) {
+	return;
+    }
+    inAction = true;
     visited = new Array();
     for (var i=0; i<board.height; i++) {
 	visited[i] = new Array();
@@ -95,7 +146,7 @@ function clickSquare(x,y) {
 	    opacity: 0
 		}, 250, function() {
 	    $('.toReappear').empty().append(newVal);
-	    $('.toReappear').removeClass('toReappear').animate({
+	    $('.toReappear').removeClass('toReappear').css('background-color', 'rgb('+(newVal*68)%255+",128,"+(newVal*193)%255+")").animate({
 	    opacity: 1
 			}, 250);
 	});
@@ -114,7 +165,7 @@ function clickSquare(x,y) {
     }
     for (i = board.height - 1; i >= 0; i--) {
 	for (var j = 0; j < board.width; j++) {
-	    if (invalidBelow[i][j]) {
+	    if (invalidBelow[i][j] && board.valid[i][j]) {
 		board.valid[i][j] = false;
 		board.grid[i + invalidBelow[i][j]][j] = board.grid[i][j];
 		board.valid[i + invalidBelow[i][j]][j] = true;
@@ -130,7 +181,8 @@ function clickSquare(x,y) {
 			top: 30*newi+'px'
 			    }, 750);
 		$('#row'+i+'col'+j).attr('id','row'+newi+'col'+j);
-		$('#row'+newi+'col'+j).attr('onclick','clickSquare('+newi+','+j+');');
+		$('#row'+newi+'col'+j).attr('onmouseup','clickSquare('+newi+','+j+');');
+		$('#row'+newi+'col'+j).attr('onmousedown','mousedown('+newi+','+j+');');
 	    }
 	}
     }
@@ -147,6 +199,7 @@ function clickSquare(x,y) {
 	    }
 	}
     }
+    setTimeout(function() {inAction = false;},1000);
     
 }
 function visit(i,j,value) {
